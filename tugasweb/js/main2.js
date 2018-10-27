e2a28223177362cc63a8eac87147baa185665be8
AL Gzl <algzl1727@gmail.com>
2018-10-27 19:45:17 +0700

    Add files via upload

---
+++ tugasweb/js/main2.js
@@ -0,0 +1,23 @@
+ window.onload = function(){
+ 	document.getElementById('tampil1').style.display = "none";
+ 	document.getElementById('tampil2').style.display = "none";
+ 	document.getElementById('tampil3').style.display = "none";
+ };
+
+ function view1() {
+ 	document.getElementById('tampil1').style.display = "block";
+ 	document.getElementById('tampil2').style.display = "none";
+ 	document.getElementById('tampil3').style.display = "none";
+ }
+
+ function view2() {
+ 	document.getElementById('tampil1').style.display = "none";
+ 	document.getElementById('tampil2').style.display = "block";
+ 	document.getElementById('tampil3').style.display = "none";
+ }
+
+ function view3() {
+ 	document.getElementById('tampil1').style.display = "none";
+ 	document.getElementById('tampil2').style.display = "none";
+ 	document.getElementById('tampil3').style.display = "block";
+ }
